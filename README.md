e072159483349eca596ce1c34d9c290a3254bcff
Jah <moskovienkor@gmail.com>
2020-11-23 12:19:10 +0200

    email-support: - Readme info added; - properties added;

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# email-support-test-task" 
+# Email support application
+##### Simple functionality to read, parse, compare and save data implemented
+
+### Getting started
+##### To start the application upload a file with information lines, write the file path into the 'app.properties' in the resources folder and run Main class.
--- src/main/java/com/email/support/Main.java
+++ src/main/java/com/email/support/Main.java
@@ -10,16 +10,28 @@ import com.email.support.service.impl.CustomLineComparatorImpl;
 import com.email.support.service.impl.CustomLineExecutorImpl;
 import com.email.support.service.impl.CustomLineParserImpl;
 import com.email.support.service.mapper.LineMapper;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 public class Main {
-    private static final String FILE_PATH = "src/main/resources/file.txt";
-
     public static void main(String[] args) {
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String appConfigPath = rootPath + "app.properties";
+
+        Properties appProps = new Properties();
+        try {
+            appProps.load(new FileInputStream(appConfigPath));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't load properties from : " + appConfigPath, e);
+        }
+
         LineMapper mapper = new LineMapper();
         CustomFileReader reader = new CustomFileReaderImpl();
         CustomLineParser parser = new CustomLineParserImpl(mapper);
-        List<Line> parsedLines = parser.parseLines(reader.readFile(FILE_PATH));
+        List<Line> parsedLines
+                = parser.parseLines(reader.readFile(appProps.getProperty("file.path")));
         CustomLineComparator comparator = new CustomLineComparatorImpl();
         CustomLineExecutor executor = new CustomLineExecutorImpl(comparator);
         System.out.println(executor.execute(parsedLines));
